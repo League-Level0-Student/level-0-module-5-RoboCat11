dab95911630dd806ba8afbb37798520e54a8622c
league <maddoxseul@sbsd.net>
2024-05-08 16:58:21 -0700

    Today I finished Goofy Names and did Snowflake.

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -11,29 +11,34 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String input = JOptionPane.showInputDialog("Please enter your name.");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+		System.out.println(input.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 7).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
+		for(int i=0; i<input.length(); i++) {
 				// 4. Create a char variable to store the next character of the name
 				//    use .charAt()
 	
-				
+		char c = input.charAt(i);		
 				// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
-			
-			
+				if(i % 2==0) {
+		c =	Character.toUpperCase(c);	
+				}
+				else {	
 				// 6. Even characters should be made uppercase and odd characters made lowercase
 				//    HINT: use Character.toUpperCase() or Character.toLowerCase()
-	
-			
+			c = Character.toLowerCase(c);
+				}
 				// 7. ADD the char to the end of the goofyName String
-
-		
-		// 8. Use pop-up to show user their Goofy name
-
+				goofyName = goofyName + c;
 	}
-}
+		// 8. Use pop-up to show user their Goofy name
+		JOptionPane.showMessageDialog(null,"Your Goofy name is " + goofyName);
+	}
+
+	
+		
+	}
 
