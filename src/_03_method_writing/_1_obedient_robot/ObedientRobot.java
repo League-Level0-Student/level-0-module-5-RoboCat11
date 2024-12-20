c82fe8ffee6987edc282a39ac1e3da66399c77e3
league <league@imac-20.attlocal.net>
2024-12-19 17:03:53 -0800

    Today I finished Obedient Robot and did Happy Pet.

--- src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
+++ src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
@@ -1,5 +1,7 @@
 package _03_method_writing._1_obedient_robot;
 
+import java.awt.Component;
+
 import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
@@ -7,56 +9,58 @@ import org.jointheleague.graphical.robot.Robot;
 public class ObedientRobot {
 
 	static Robot rob = new Robot();
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		rob.setSpeed(100);
 		rob.penDown();
-		while(true) {
-		String input = JOptionPane.showInputDialog("What shape do you want?");
-		String color = JOptionPane.showInputDialog("What color do you want? You can either pick red, green, or blue.");
-			if(input.contains("red")) {
-		rob.setPenColor(255,0,0);	
-		}
-		if(input.contains("green")) {
-			rob.setPenColor(0,255,0);
-		}
-		if(input.contains("blue")) {
-			rob.setPenColor(0,0,255);	
-		}
-		if(input.contains("square")) {
-		drawSquare();
-		}
-		if(input.contains("triangle")) {
-		drawTriangle();	
-		}
-		if(input.contains("circle")) {
-		drawCircle();	
-	
-	}
+			for(int i=0; i<1; i++) {
+			String input = JOptionPane.showInputDialog("What shape do you want?");
+			String color = JOptionPane
+					.showInputDialog("What color do you want? You can either pick red, green, or blue.");
 		
-	}
+			if (color.contains("red")) {
+				rob.setPenColor(255, 0, 0);
+			}
+			if (color.contains("green")) {
+				rob.setPenColor(0, 255, 0);
+			}
+			if (color.contains("blue")) {
+				rob.setPenColor(0, 0, 255);
+			}
+			if (input.contains("square")) {
+				drawSquare();
+			}
+			if (input.contains("triangle")) {
+				drawTriangle();
+			}
+			if (input.contains("circle")) {
+				drawCircle();
+				}
+			}
+		}
 
-		// TODO Auto-generated method stub
-		
-	}
+
+
 	private static void drawCircle() {
 		for(int i=0; i<36; i++) {
 		rob.move(50);
 		rob.turn(10);
 		}
 	}
-	static  void drawSquare() {
-		for(int i=0; i<4; i++) {
+
+	static void drawSquare() {
+		for (int i = 0; i < 4; i++) {
 			rob.move(200);
 			rob.turn(90);
 		}
 	}
+
 	static void drawTriangle() {
-		for(int i=0; i<3; i++) {
+		for (int i = 0; i < 3; i++) {
 
 			rob.move(500);
 			rob.turn(120);
 		}
 	}
 }
-
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,18 +5,20 @@ public class HappyPet {
 	
 	// 1. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel =0;
 	public static void main(String[] args) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		String input = JOptionPane.showInputDialog("What kind of pet would like to buy? I have turtles, dogs, cats, hummingbirds, coyotes, and wolves!");
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
 		
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
+
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
 			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+					new String[] { "Feed", "Button2", "Button3" }, null);
 
+			System.out.println(task);
 			// 6. Use user input to call the appropriate method created in step 5 below.
 
 			// 7. If you determine the happiness level is large enough, tell the
